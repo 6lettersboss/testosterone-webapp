2d00f4d4c6af362b51c0c862f2b0ca1081a60b55
6lettersboss <sungjin.kim@6letters.co.kr>
2025-05-19 15:39:53 +0900

    Delete testosterone-webapp 5 directory

--- testosterone-webapp 5/utils/algorithm.ts
+++
@@ -1,7 +0,0 @@
-
-export function estimateTestosterone(input: { hrv: number; age: number }): number {
-  const base = 500;
-  const hrvFactor = input.hrv * 2;
-  const agePenalty = input.age * 3;
-  return base + hrvFactor - agePenalty;
-}
